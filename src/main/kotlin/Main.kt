5ea319a3713f33ad9e2ddf40f911f67612359e4b
dduckkhangg <duckhang0702@gmail.com>
2023-04-18 22:02:55 +0700

    study for kotlin comment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
 fun main() {
+    /*
+    * study for kotlin core
+    * */
+    /// this is comment
     println("Hello word: Khang")
     println(17 + 8)
     println("Tada : ${3+3}")
 
+    // comment for different print and println
     print("Phan biet 2 phuong thuc print va println")
 }
